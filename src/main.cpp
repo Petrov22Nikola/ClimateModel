bd2d956c5b38a47f29b062e328b96c13131a7c9a
Nikola Petrov <nikola.petrov@coherent.global>
2025-05-06 00:29:35 -0400

    Import & link GLAD & GLFW libraries & window setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,55 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Keyboard input
+void processInput(GLFWwindow *window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// Framebuffer resizing
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
 
 int main(int argc, char **argv) {
-    std::cout << "Hello world!" << std::endl;
+    // Initialization
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // Window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Climate Simulation", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }    
+
+    // Register framebuffer resizing callback & set framebuffer size
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    // Event loop
+    while(!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+  
+    glfwTerminate();
     return 0;
 }
