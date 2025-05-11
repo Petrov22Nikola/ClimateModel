46d6386ae912e97abf4def773d98c43d4016135a
Nikola Petrov <nikola.petrov@coherent.global>
2025-05-11 14:30:29 -0400

    Curl weather query

--- src/dataScanner.cpp
+++ src/dataScanner.cpp
@@ -4,10 +4,19 @@
 
 #include <core/dataScanner.h>
 
-// Curl received data write callback
-size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
+// Curl thermal PNG received data write callback
+size_t thermal_write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
     std::ofstream thermalImage;
     thermalImage.open("thermalImage.png", std::ios::out | std::ios::app | std::ios::binary);
+    thermalImage.write(ptr, nmemb);
+    thermalImage.close();
+    return nmemb;
+}
+
+// Curl weather JSON recevied data write callback
+size_t weather_write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
+    std::ofstream thermalImage;
+    thermalImage.open("weatherData.txt", std::ios::out | std::ios::app);
     thermalImage.write(ptr, nmemb);
     thermalImage.close();
     return nmemb;
@@ -32,12 +41,48 @@ void thermalData() {
     if(curlHandle) {
         CURLcode res;
         curl_easy_setopt(curlHandle, CURLOPT_URL, thermalLink.c_str());
-        curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, &write_callback);
+        curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, &thermal_write_callback);
         res = curl_easy_perform(curlHandle);
         if (res == CURLE_OK) {
-            std::cout << "Curl executed successfully." << std::endl;
+            std::cout << "Curl thermal query executed successfully." << std::endl;
         } else {
-            std::cout << "Curl encountered an error: " << res << std::endl;
+            std::cout << "Curl thermal query encountered an error: " << res << std::endl;
+        }
+        curl_easy_cleanup(curlHandle);
+    }
+}
+
+void weatherData() {
+    // Avoid fetching duplicate data
+    std::ifstream weatherFile("weatherData.txt");
+    if (weatherFile.good()) return;
+    std::string weatherLink = "https://api.open-meteo.com/v1/forecast?"
+                              "latitude=43.4675"
+                              "&longitude=-79.6877"
+                              "&current=is_day,"
+                              "apparent_temperature,"
+                              "relative_humidity_2m,"
+                              "temperature_2m,"
+                              "precipitation,"
+                              "rain,showers,"
+                              "snowfall,"
+                              "weather_code,"
+                              "cloud_cover,"
+                              "pressure_msl,"
+                              "surface_pressure,"
+                              "wind_speed_10m,"
+                              "wind_gusts_10m,"
+                              "wind_direction_10m";
+    CURL *curlHandle = curl_easy_init();
+    if(curlHandle) {
+        CURLcode res;
+        curl_easy_setopt(curlHandle, CURLOPT_URL, weatherLink.c_str());
+        curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, &weather_write_callback);
+        res = curl_easy_perform(curlHandle);
+        if (res == CURLE_OK) {
+            std::cout << "Curl weather query executed successfully." << std::endl;
+        } else {
+            std::cout << "Curl weather query encountered an error: " << res << std::endl;
         }
         curl_easy_cleanup(curlHandle);
     }
@@ -46,4 +91,5 @@ void thermalData() {
 void initializeData() {
     std::cout << "Scanning data sources." << std::endl;
     thermalData();
+    weatherData();
 }
