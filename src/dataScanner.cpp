43fe547fb25e7e23e0375066c66387d981f97f58
Nikola Petrov <nikola.petrov@coherent.global>
2025-05-11 15:05:41 -0400

    Reformat query functions to accept dates & latitudes / longitudes

--- src/dataScanner.cpp
+++ src/dataScanner.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
 #include <curl/curl.h>
 
 #include <core/dataScanner.h>
+
+const int epochTime = 1900;
 
 // Curl thermal PNG received data write callback
 size_t thermal_write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
@@ -22,21 +28,26 @@ size_t weather_write_callback(char *ptr, size_t size, size_t nmemb, void *userda
     return nmemb;
 }
 
-void thermalData() {
+void thermalData(tm date) {
     // Avoid fetching duplicate data
     std::ifstream thermalFile("thermalImage.png");
     if (thermalFile.good()) return;
-    std::string thermalLink = "https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?"
-                              "SERVICE=WMS"
-                              "&VERSION=1.3.0"
-                              "&REQUEST=GetMap"
-                              "&LAYERS=MODIS_Terra_Land_Surface_Temp_Day"
-                              "&TIME=2025-05-09"
-                              "&CRS=EPSG:4326"
-                              "&BBOX=-90,-180,90,180"
-                              "&WIDTH=2048"
-                              "&HEIGHT=1024"
-                              "&FORMAT=image/png";
+    std::ostringstream oss;
+    oss << "https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?"
+        << "SERVICE=WMS"
+        << "&VERSION=1.3.0"
+        << "&REQUEST=GetMap"
+        << "&LAYERS=MODIS_Terra_Land_Surface_Temp_Day"
+        << "&TIME="
+        << (epochTime + date.tm_year) << "-"
+        << std::setw(2) << std::setfill('0') << (1 + date.tm_mon) << "-"
+        << std::setw(2) << std::setfill('0') << date.tm_mday
+        << "&CRS=EPSG:4326"
+        << "&BBOX=-90,-180,90,180"
+        << "&WIDTH=2048"
+        << "&HEIGHT=1024"
+        << "&FORMAT=image/png";
+    std::string thermalLink = oss.str();
     CURL *curlHandle = curl_easy_init();
     if(curlHandle) {
         CURLcode res;
@@ -52,27 +63,31 @@ void thermalData() {
     }
 }
 
-void weatherData() {
+void weatherData(double latitude, double longitude) {
     // Avoid fetching duplicate data
     std::ifstream weatherFile("weatherData.txt");
     if (weatherFile.good()) return;
-    std::string weatherLink = "https://api.open-meteo.com/v1/forecast?"
-                              "latitude=43.4675"
-                              "&longitude=-79.6877"
-                              "&current=is_day,"
-                              "apparent_temperature,"
-                              "relative_humidity_2m,"
-                              "temperature_2m,"
-                              "precipitation,"
-                              "rain,showers,"
-                              "snowfall,"
-                              "weather_code,"
-                              "cloud_cover,"
-                              "pressure_msl,"
-                              "surface_pressure,"
-                              "wind_speed_10m,"
-                              "wind_gusts_10m,"
-                              "wind_direction_10m";
+    std::ostringstream oss;
+    oss << "https://api.open-meteo.com/v1/forecast?"
+        << "latitude=" 
+        << std::fixed << std::setprecision(4) << latitude
+        << "&longitude="
+        << std::fixed << std::setprecision(4) << longitude
+        << "&current=is_day,"
+        << "apparent_temperature,"
+        << "relative_humidity_2m,"
+        << "temperature_2m,"
+        << "precipitation,"
+        << "rain,showers,"
+        << "snowfall,"
+        << "weather_code,"
+        << "cloud_cover,"
+        << "pressure_msl,"
+        << "surface_pressure,"
+        << "wind_speed_10m,"
+        << "wind_gusts_10m,"
+        << "wind_direction_10m";
+    std::string weatherLink = oss.str();
     CURL *curlHandle = curl_easy_init();
     if(curlHandle) {
         CURLcode res;
@@ -90,6 +105,8 @@ void weatherData() {
 
 void initializeData() {
     std::cout << "Scanning data sources." << std::endl;
-    thermalData();
-    weatherData();
+    time_t timestamp = time(&timestamp);
+    struct tm datetime = *localtime(&timestamp);
+    thermalData(datetime);
+    weatherData(43.4675, -79.6877);
 }
